298f195096dd09f827dbc931f99b23ae4499ee73
Phoomparin Mano <phoomparin@gmail.com>
2025-02-11 00:38:24 +0700

    make default import paths reasonable

--- enterprise/frontend/src/embedding-sdk/cli/utils/get-suggested-import-path.ts
+++ enterprise/frontend/src/embedding-sdk/cli/utils/get-suggested-import-path.ts
@@ -3,13 +3,23 @@ import path from "path";
 import { GENERATED_COMPONENTS_DEFAULT_PATH } from "../constants/config";
 
 export const getSuggestedImportPath = (componentDir?: string): string => {
-  // We don't know where the user will import the component from.
-  // We assume they will import from their components directory,
-  // so we use the last directory in the path as an example.
-  // e.g. "./src/components/metabase" -> "./metabase".
-  const importPath = path.basename(
-    componentDir || GENERATED_COMPONENTS_DEFAULT_PATH,
-  );
+  const input = componentDir || GENERATED_COMPONENTS_DEFAULT_PATH;
 
-  return importPath.startsWith(".") ? importPath : `./${importPath}`;
+  if (input === ".") {
+    return "..";
+  }
+
+  const normalized = path.normalize(input).replace(/^\.\//, "");
+  const parts = normalized.split("/");
+
+  if (parts.length === 1) {
+    return `../${parts[0]}`;
+  }
+
+  // We do not want to include the "src" directory in the import path.
+  if (parts[0] === "src") {
+    parts.shift();
+  }
+
+  return `../${parts.join("/")}`;
 };
